f14e4f207a5b4a1a8a7e51d9bd9434ae96e57687
HazeGrey26 <147719792+HazeGrey26@users.noreply.github.com>
2023-11-15 09:46:42 -0500

    Sprite system is a work in progress. Am able to draw sprites on screen and move them in 3D space. Really buggy at the moment.

--- Floorcasting Rev 7/sprite_classes.py
+++ Floorcasting Rev 7/sprite_classes.py
@@ -1,6 +1,7 @@
-from math import pi, atan
+from math import pi, asin, atan, acos, cos, sin
 import pygame
-from numpy import deg2rad
+from numpy import deg2rad, dot
+import numpy
 from settings import *
 
 
@@ -30,30 +31,43 @@ class Enemy(pygame.sprite.Sprite):
         else:
             return False  # Not Dead
 
+
     def draw_enemy(self, surface, hres, rot, player_pos_y, player_pos_x, halfvres):
+        rot = rot - pi/2  # Sets rot to 0 when player is facing along the positive y-axis
         player_rot = 2*pi - (rot % (2 * pi))  # Caps player rot to 2pi and makes it increase towards counterclockwise
         fov = deg2rad(FIELD_OF_VIEW/2)  # Converts the player's field of view to radians and divides it by 2
         delta_x = player_pos_x - self.position[0]
         delta_y = player_pos_y - self.position[1]
-        distance_to_player = (delta_x**2 + delta_y**2)**(1/2)  # Pythagorean Theorem
-        angle_to_player = atan(delta_y / delta_x) + pi/2
-        delta_angle = player_rot - angle_to_player  # Negative when enemy is to the left of where the player is looking
+        magnitude = 1/((delta_x**2 + delta_y**2)**(1/2))  # Magnitude of a vector formula
+        distance_to_player = (delta_x ** 2 + delta_y ** 2) ** (1 / 2)  # Pythagorean Theorem
+        player_vector = [cos(player_rot), sin(player_rot)]
+        rotation_vector = [cos(2*pi - ((rot - pi/2) % (2 * pi))), sin(2*pi - ((rot - pi/2) % (2 * pi)))]
+        enemy_vector = [-delta_x * magnitude, -delta_y * magnitude]
+        angle_between_vectors = numpy.dot(player_vector, enemy_vector)
 
-        print(f"Dist: {distance_to_player}, Angle: {angle_to_player}, Rot: {player_rot}, Coord: {delta_x},{delta_y}")
+        if enemy_vector[0] < 0:
+            angle_to_player = asin(delta_y / distance_to_player)
+            print("asin")
+        else:
+            angle_to_player = pi - asin(delta_y / distance_to_player)
+            print("alt asin")
+        delta_angle = player_rot - angle_to_player  # This is where my sprite problem is
 
-        self.image_scale = abs(1 / (distance_to_player**(1/2)))  # This is a test
+        self.image_scale = abs(1 / (((delta_x**2 + delta_y**2)**(1/2)) ** (1 / 2)))   # This is a test
         self.sprite_size = (int(self.SOURCE_IMAGE_RESOLUTION[0] * self.image_scale),
                             int(self.SOURCE_IMAGE_RESOLUTION[1] * self.image_scale))
         self.image = pygame.transform.scale(self.SOURCE_IMAGE, self.sprite_size)
         screen_pos = (self.enemy_screen_pos[0] - self.image.get_height() / 2, self.enemy_screen_pos[1] - self.image.get_width() / 2)
 
-        angle_to_screen = delta_angle/fov  # Where the enemy will be drawn on screen (negative = left, positive = right)
+        screen_shift = (delta_angle - pi)/fov  # Where the enemy will be drawn on screen (negative = left, positive = right)
+
         # Shifts the enemies y-position on screen according to angle_to_screen
-        screen_pos = (screen_pos[0] + angle_to_screen * (SCREEN_RES[0]/2), screen_pos[1])
+        screen_pos = (screen_pos[0] + screen_shift * (SCREEN_RES[0]/2), screen_pos[1])
+        print(f"P_Rot: {int(player_rot)} dlta {delta_angle}\nplrAng {angle_to_player}\nEVec {enemy_vector}")
 
-        if -1 < angle_to_screen < 1:  # Ensures the enemy is drawn only when the player can see it
+        if angle_between_vectors > 5/6:  # Ensures the enemy is drawn only when the player can see it
             surface.blit(self.image, screen_pos)  # Draws the enemy on screen
-
+            print("True")
 
 
 enemies = pygame.sprite.Group()
